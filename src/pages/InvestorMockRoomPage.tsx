be255af6b840831db65c44f730519eb31550b6a6
Harshvardhan295 <harshvardhankhajuria14@gmail.com>
2025-09-28 14:28:09 +0530

    fixed final improvements

--- src/pages/InvestorMockRoomPage.tsx
+++ src/pages/InvestorMockRoomPage.tsx
@@ -1,19 +1,15 @@
 import { InvestorMockRoom } from "@/components/investor-mockroom/InvestorMockRoom";
-import { Button } from "@/components/ui/button";
-import { Link } from 'react-router-dom';
 import { Header } from "@/components/Header";
 
 export default function InvestorMockRoomPage() {
   return (
-    <div className="min-h-screen bg-background flex flex-col">
+    <div className="min-h-screen bg-background flex flex-col overflow-x-hidden">
       {/* Header */}
       <Header />
-      {/* Main content: take all remaining space, center contents */}
-      <main className="flex-1 flex flex-col justify-center items-center bg-gradient-to-tr from-blue-50 via-white to-blue-100 p-4">
+      {/* Main content */}
+      <main className="container mx-auto p-4 md:p-8 w-full flex-1 flex flex-col justify-center items-center bg-gradient-to-tr ">
         <InvestorMockRoom />
       </main>
     </div>
   );
 }
-
-
