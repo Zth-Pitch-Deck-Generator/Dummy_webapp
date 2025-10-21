a568e41460fa7f4d08269cd9c6d389436a650d7d
Adnan <studentprogrammer3.com>
2025-10-21 15:52:04 +0530

    implemented google auth

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
+// Error checking (Good practice!)
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL and Anon Key must be set in your environment variables.");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
